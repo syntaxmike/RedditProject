19b3caac07f3127e3b1d6230e759fd2c8cf50866
Michael Oceguera <mocegue8@calstatela.edu>
2018-03-23 23:09:21 -0700

    Update cli.js

--- reddit-app/cli.js
+++ reddit-app/cli.js
@@ -4,19 +4,35 @@ const
 
 
 const flags = yargs.usage('$0: Usage <cmd> [options]')
-      .command({
+    .command({
         command: 'search',
-        desc: 'Search for Sub-Reddits',
+        desc: 'Search for Sub-Reddit',
         builder: (yargs) => {
             return yargs.option('name', {
                 alias: 'n',
-                describe: 'Search By Sub-Reddit name'
-            }).option('count', {
+                describe: 'Search by Sub-Reddit name'
+            })
+            .option('count', {
                 alias: 'c',
                 describe: 'Number of Sub-Reddits to display in search'
             })
+            .option('topic', {
+                alias: 't',
+                describe: 'Search for Sub-Reddits topic'
+            })
+            
         },
-        handler: (argv) => { app.interestSearch(argv.name, argv.count) }
+        handler: (argv) => { 
+            if(argv.topic){
+                app.topic(argv.topic)
+            }else{
+                app.interestSearch(argv.name, argv.count) 
+            }}
+    })
+     .command({
+        command: 'popular',
+        desc: 'Search for Popular Sub-Reddits',
+        handler: (argv) => { app.popular() }
     })
     .help('help')
     .argv
