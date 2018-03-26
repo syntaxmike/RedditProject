ec42a628647843ff046a490dc4170b44ae61dbdb
Michael Oceguera <mocegue8@calstatela.edu>
2018-03-25 17:34:01 -0700

    Update redditApp.js

--- reddit-app/redditApp.js
+++ reddit-app/redditApp.js
@@ -24,35 +24,34 @@ const selectionPrompt = (results) => {
                 
         }
     },{
-        type: 'list',
+        type: 'input',
         name: 'toshow',
-        message: 'Select which threads to display: ',
-        choices: ["top", "hot"],
+        message: 'Enter which threads to display hot or top: ',
         validate: (answers) => {
     
-            if(answers){
+            if(answers.toLowerCase() == "hot" || answers.toLowerCase() == "top"){
                 return true
             }
     
             else{
-                return 'Something went wrong'
+                return 'Please enter hot or top'
             }
                 
         }
     }
     ])
     .then((answers) => {
-        displayItem(answers.results, answers.toshow)
+        displayItem(answers.results, answers.toshow.toLowerCase())
     })
 }
 
 //Displays sub-reddit's top threads at the moment
-const displayItem = (id, show) =>{
+const displayItem = (id, threadType) =>{
 
     let table = new Table({ head: [`${id}`]})
 
     //if top is selected show this, else show hot threads
-    redditAPI.idSearch(id, show)
+    redditAPI.idSearch(id, threadType)
         .then(idResult => {
             
             for(let index in idResult.data.children){
@@ -86,6 +85,7 @@ const displayItem = (id, show) =>{
 //Initial name start
 const interestSearch = (interest, count = 20) => {
     let subReddits = []
+
     redditAPI.search(interest, count)
         .then(results => {
             for(let index in results.data.children){
@@ -99,29 +99,30 @@ const interestSearch = (interest, count = 20) => {
 //Initial topic start
 const topic = (interest) => {
     let subReddits = []
+
     redditAPI.byTopic(interest)
-    .then(results => {
-        for(let index in results){
-            subReddits.push(results[index])
-        }
-         selectionPrompt(subReddits)
-    })
-    .catch(err => console.log(err))
+        .then(results => {
+            for(let index in results){
+                subReddits.push(results[index])
+            }
+            selectionPrompt(subReddits)
+        })
+        .catch(err => console.log(err))
 }
 
 //Initial start for popular search
 const popular = () => {
     let subReddits = []
-    redditAPI.popular()
-    .then(results => {
-        for(let index in results.data.children){
-            subReddits.push(results.data.children[index].data)
-        }
-        selectionPrompt(subReddits)
-    })
-    .catch(err => console.log(err))
-}
 
+    redditAPI.popular()
+        .then(results => {
+            for(let index in results.data.children){
+                subReddits.push(results.data.children[index].data)
+            }
+            selectionPrompt(subReddits)
+        })
+        .catch(err => console.log(err))
+}
 
 module.exports = {
     interestSearch, 
